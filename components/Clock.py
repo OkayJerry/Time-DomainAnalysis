5b8350f3a4ef5c4c89b878f6d52858c1a0dddf83
Jerry Hoskins <70593138+OkayJerry@users.noreply.github.com>
2023-11-13 15:02:45 -0500

    Delete components directory

--- components/Clock.py
+++
@@ -1,9 +0,0 @@
-from PyQt6.QtCore import QTimer
-
-class Clock(QTimer):
-    def __init__(self):
-        super().__init__()
-        
-    def updateInterval(self, hz: float):
-        interval = int(1000 / hz)
-        self.setInterval(interval)
